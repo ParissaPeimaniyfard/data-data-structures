0c0371750a89a528329b14e954e4ceb303d40270
Parissa Peimaniyfard <zahra.pmf@gmail.com>
2023-10-22 15:02:26 +0200

    style cghange

--- currencies.py
+++ currencies.py
@@ -1,6 +1,5 @@
 # pylint: disable=missing-docstring
 
-# TODO: add some currency rates
 RATES = {
     "USDEUR": 0.85,
     "GBPEUR": 1.13,
@@ -13,10 +12,14 @@ def convert(amount, currency):
     amount is a tuple like (100, "EUR")
     currency is a string
     """
+
     for key, value in RATES.items():
         if amount[1]==key[:3] and currency==key[3:6]:
             return round(amount[0]*value)
         if amount[1]==key[3:6] and currency==key[:3]:
             return round(amount[0]/value)
+
+
+
 #amount=(100, "EUR")
 #print(convert(amount,"hgb"))
